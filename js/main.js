9f9388ecca06e9bf57ed79be7c8abe1d7d53467a
Kreshnik Gashi <kreshnnikgashi@gmail.com>
2022-05-20 02:58:08 +0200

    Projects Section

--- js/main.js
+++ js/main.js
@@ -25,15 +25,18 @@ const btnOpen = document.getElementById('btnHmaburger');
 const btnClose = document.querySelector('#closeMenu');
 const menuEl = document.getElementById('menu');
 const overlayEl = document.getElementById('overlay');
+const bodyEl = document.querySelector('body');
 
 const openMenu = () => {
   menuEl.classList.remove('closed');
   overlayEl.classList.remove('hidden');
+  bodyEl.classList.add('noscroll');
 };
 
 const closeMenu = () => {
   menuEl.classList.add('closed');
   overlayEl.classList.add('hidden');
+  bodyEl.classList.remove('noscroll');
 };
 
 btnHamburger.addEventListener('click', openMenu);
@@ -56,3 +59,21 @@ const fixNav = () => {
 };
 
 window.addEventListener('scroll', fixNav);
+
+//* =============================== News Slider =============================
+const swiperNews = new Swiper('.news-slider', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
